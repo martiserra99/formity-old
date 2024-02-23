d95765dc6d091f8d5f78614099fd5927020ac1db
Marti Serra <martiserramolina99@gmail.com>
2024-02-23 17:02:07 +0100

    Added comments

--- src/types/value.ts
+++ src/types/value.ts
@@ -1,11 +1,22 @@
 import { Value } from 'mongu';
 
+/**
+ * It represents a form.
+ */
 type ValueForm = {
-  defaultValues: { [key: string]: Value };
-  resolver: { [key: string]: [Value, string][] };
-  render: Value[];
+  defaultValues: { [key: string]: Value }; // Default values
+  resolver: { [key: string]: [Value, string][] }; // Validation rules (expressions that evaluate to booleans, and error messages)
+  render: Value[]; // What is rendered (we can only reference the available components)
 };
+
+/**
+ * It represents what is returned.
+ */
 type ValueReturn = Value;
+
+/**
+ * It represents variables. Each property corresponds to a variable.
+ */
 type ValueVariables = { [key: string]: Value };
 
 export { ValueForm, ValueReturn, ValueVariables };
