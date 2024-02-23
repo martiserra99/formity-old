f3e39dcf444b06a041370849b8b04bf8c5cc66f3
Marti Serra <martiserramolina99@gmail.com>
2024-02-23 13:18:53 +0100

    Added comments at json.ts

--- src/types/json.ts
+++ src/types/json.ts
@@ -1,13 +1,48 @@
 import { Value } from 'mongu';
 
+/**
+ * It represents the union of list, condition, loop, form, return and variables.
+ */
 type Json = JsonFlow | JsonItem;
+
+/**
+ * It represents the union of list, condition and loop.
+ */
 type JsonFlow = JsonList | JsonCond | JsonLoop;
+
+/**
+ * It represents a list.
+ */
 type JsonList = (JsonCond | JsonLoop | JsonItem)[];
+
+/**
+ * It represents a condition.
+ */
 type JsonCond = { cond: { if: Value; then: JsonList; else: JsonList } };
+
+/**
+ * It represents a loop.
+ */
 type JsonLoop = { loop: { while: Value; do: JsonList } };
+
+/**
+ * It represents the union of form, return and variables.
+ */
 type JsonItem = JsonForm | JsonReturn | JsonVariables;
+
+/**
+ * It represents a form.
+ */
 type JsonForm = { form: Value };
+
+/**
+ * It represents what is returned.
+ */
 type JsonReturn = { return: Value };
+
+/**
+ * It represents variables.
+ */
 type JsonVariables = { variables: Value };
 
 export {
